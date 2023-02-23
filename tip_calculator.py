8a59e3704e3ec8319d296b818664df7d254fc078
A-driana <112205906+A-driana@users.noreply.github.com>
2023-02-23 17:20:59 +0100

    Add files via upload

---
+++ tip_calculator.py
@@ -0,0 +1,11 @@
+# Little tip calculator.
+
+print("Welcome to the tip calculator for bat mathematician.")
+bill = float(input("What is the total bill?\n"))
+tip = int(input("How much tip would you like to give in %?\n"))
+people = int(input("How many people to split the bill?\n"))
+
+tip_calculation = 1 + (tip / 100)
+one_person_payment = (bill / people) * tip_calculation
+payment = "%0.2f" % (one_person_payment)
+print(f"Each person should pay {payment}.")
