03f069fefe96ca83b6e1c23310b28419ba75d2fd
A-driana <112205906+A-driana@users.noreply.github.com>
2023-02-23 16:35:20 +0100

    Add files via upload

---
+++ life_in_weeks.py
@@ -0,0 +1,12 @@
+# Do you wanna know how much time do you have considering you will live till 90?
+# Welcome in my little calculator.
+# Dont get paranoid...
+
+age = input("What is your current age? ")
+
+years_till_death = 90 - int(age)
+months_till_death = years_till_death * 12
+weeks_till_death = years_till_death * 52
+days_till_death = years_till_death * 365
+
+print(f"You have {days_till_death} days, {weeks_till_death} weeks, and {months_till_death} months left.")
