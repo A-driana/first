efb3a13c4b396dbd8ad81a4d974ab2fd232a03d9
A-driana <112205906+A-driana@users.noreply.github.com>
2023-02-24 15:29:10 +0100

    Update band_name_generator.py

--- band_name_generator.py
+++ band_name_generator.py
@@ -1,6 +1,7 @@
-print("Welcome to the band name generator!")
+# # TODO - Band name generator
+# print("Welcome to the band name generator!")
 
-city = input("What is the name of city you grew up in?\n")
-pet_name = input("What is the name of your pet?\n")
+# city = input("What is the name of city you grew up in?\n")
+# pet_name = input("What is the name of your pet?\n")
 
-print("The name of your rock band could be " + city + " " + pet_name + ".")
+# print("The name of your rock band could be " + city + " " + pet_name + ".")
