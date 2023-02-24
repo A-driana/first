83270266912737bb8bc4282783becf00665ade83
A-driana <112205906+A-driana@users.noreply.github.com>
2023-02-24 15:28:32 +0100

    Update BMI_calculator.py

--- BMI_calculator.py
+++ BMI_calculator.py
@@ -1,9 +1,19 @@
-initial_height = input("Enter your height in m: ")
-initial_weight = input("Enter your weight in kg: ")
+# # TODO - BMI calculator
+# initial_height = input("Enter your height in m: ")
+# initial_weight = input("Enter your weight in kg: ")
 
-height = float(initial_height)
-weight = int(initial_weight)
+# height = float(initial_height)
+# weight = int(initial_weight)
 
-result = weight / (height * height)
+# bmi = round(weight / (height ** 2))
 
-print(int(result))
+# if bmi <= 18.5:
+#     print(f"Your BMI is {bmi}, you are underweight.")
+# elif bmi <= 25:
+#     print(f"Your BMI is {bmi}, you have a normal weight.")
+# elif bmi <= 20:
+#     print(f"Your BMI is {bmi}, you are slightly overweight.")
+# elif bmi <= 40:
+#     print(f"Your BMI is {bmi}, you are obese.")
+# else:
+#     print(f"Your BMI is {bmi}, you are clinically obese.")
