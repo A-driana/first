f9115fab19e360d5a88aa72e1d1f5c81b3ca4e9f
A-driana <112205906+A-driana@users.noreply.github.com>
2023-02-24 15:29:41 +0100

    Update life_in_weeks.py

--- life_in_weeks.py
+++ life_in_weeks.py
@@ -1,12 +1,13 @@
-# Do you wanna know how much time do you have considering you will live till 90?
-# Welcome in my little calculator.
-# Dont get paranoid...
+# # TODO - Time left calculator
+# # Do you wanna know how much time do you have considering you will live till 90?
+# # Welcome in my little calculator.
+# # Dont get paranoid...
 
-age = input("What is your current age? ")
+# age = input("What is your current age? ")
 
-years_till_death = 90 - int(age)
-months_till_death = years_till_death * 12
-weeks_till_death = years_till_death * 52
-days_till_death = years_till_death * 365
+# years_till_death = 90 - int(age)
+# months_till_death = years_till_death * 12
+# weeks_till_death = years_till_death * 52
+# days_till_death = years_till_death * 365
 
-print(f"You have {days_till_death} days, {weeks_till_death} weeks, and {months_till_death} months left.")
+# print(f"You have {days_till_death} days, {weeks_till_death} weeks, and {months_till_death} months left.")
