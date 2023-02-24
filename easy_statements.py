9b9166e748ea98eee76cfd1549ad86874117d019
A-driana <112205906+A-driana@users.noreply.github.com>
2023-02-24 17:10:09 +0100

    Update easy_statements.py

--- easy_statements.py
+++ easy_statements.py
@@ -1,25 +1,26 @@
-# # TODO - Even or odd number
-# number = int(input("Which number do you want to check? "))
-
-# if number % 2 == 0:
-#     print("This is an even number.")
-# else:
-#     print("This is an odd number.")
-
-
 # # TODO - Nested statement
 # print("Welcome to the roller coaster!\n")
 
 # height = int(input("What is your height in cm?\n"))
 # age = int(input("What is your age?\n"))
+# bill = 0
 
 # if height >= 120:
-#     print("You can ride the roller coaster!")
+#     # print("You can ride the roller coaster!")
 #     if age < 12:
-#         print("Please, pay 5€.")
+#         bill = 5
+#         # print("Child ticket is 5€.")
 #     elif age <= 18:
-#         print("Please, pay 7€.")
+#         bill = 7
+#         # print("Youth ticket is 7€.")
 #     else:
-#         print("Please, pay 12€.")
+#         bill = 12
+#         # print("Adult ticket is 12€.")
+
+#     photo = input("Do you want a photo taken for 3€? Y or N. ")
+#     if photo == "Y":
+#         bill += 3
+
+#     print(f"Your final bill is {bill}€.")
 # else:
 #     print("Sorry, you are not tall enough.")
