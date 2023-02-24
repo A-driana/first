8aa8e7236ae6775635f3f45505e30159bbff0c0a
A-driana <112205906+A-driana@users.noreply.github.com>
2023-02-24 15:30:15 +0100

    Update tip_calculator.py

--- tip_calculator.py
+++ tip_calculator.py
@@ -1,11 +1,12 @@
-# Little tip calculator.
+# # Little tip calculator.
+# print("Welcome to the tip calculator for every bad mathematician.")
 
-print("Welcome to the tip calculator for bat mathematician.")
-bill = float(input("What is the total bill?\n"))
-tip = int(input("How much tip would you like to give in %?\n"))
-people = int(input("How many people to split the bill?\n"))
+# bill = float(input("What is the total bill?\n"))
+# tip = int(input("How much tip would you like to give in %?\n"))
+# people = int(input("How many people to split the bill?\n"))
 
-tip_calculation = 1 + (tip / 100)
-one_person_payment = (bill / people) * tip_calculation
-payment = "%0.2f" % (one_person_payment)
-print(f"Each person should pay {payment}.")
+# tip_calculation = 1 + (tip / 100)
+# one_person_payment = (bill / people) * tip_calculation
+# payment = "%0.2f" % (one_person_payment)
+
+# print(f"Each person should pay {payment}.")
