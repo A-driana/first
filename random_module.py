31e8108f8dc91aca651d0194e6baed88fda97721
A-driana <112205906+A-driana@users.noreply.github.com>
2023-02-25 19:05:57 +0100

    Add files via upload

---
+++ random_module.py
@@ -0,0 +1,15 @@
+# # TODO Using random module examples
+# import random
+# random_integer = random.randint(1, 10)
+# print(random_integer)
+#
+# random_float = random.random() * 5
+# print(random_float)
+
+# # TODO - Heads or Tails
+# import random
+# random_side = random.randint(1, 2)
+# if random_side == 1:
+#     print("Heads")
+# else:
+#     print("Tails")
