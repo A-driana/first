aa8f0aa65c470d219ee5b6a76e0c7bc5f5b6daba
A-driana <112205906+A-driana@users.noreply.github.com>
2023-02-25 19:05:43 +0100

    Add files via upload

---
+++ roulette.py
@@ -0,0 +1,10 @@
+# # TODO Banker Roulette - Who will pay the bill?
+# import random
+#
+# names_string = input("Give me everybody's names, separated by a comma.\n")
+# names = names_string.split(", ")
+#
+# total = len(names)
+# random_choice = random.randint(0, total - 1)
+# who_will_pay = names[random_choice]
+# print(who_will_pay + " is going to pay today's bill.")
