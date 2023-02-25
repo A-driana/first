b66b765329d7416adef8f96673f715091f3f07ed
A-driana <112205906+A-driana@users.noreply.github.com>
2023-02-25 19:06:23 +0100

    Create love_calculator.py

---
+++ love_calculator.py
@@ -0,0 +1,33 @@
+# # TODO - Love Calculator
+# # Let's find if you are ment together.
+#
+# print("Welcome to the Love Calculator!")
+# name1 = input("What is your name? \n")
+# name2 = input("What is their name? \n")
+#
+# two_names = name1 + name2
+# lower_case_string = two_names.lower()
+#
+# t = lower_case_string.count("t")
+# r = lower_case_string.count("r")
+# u = lower_case_string.count("u")
+# e = lower_case_string.count("e")
+#
+# true = t + r + u + e
+#
+# l = lower_case_string.count("l")
+# o = lower_case_string.count("o")
+# v = lower_case_string.count("v")
+# e = lower_case_string.count("e")
+#
+# love = l + o + v + e
+#
+# love_score = str(true) + str(love)
+# score = int(love_score)
+#
+# if score <= 10 or score >= 90:
+#     print(f"Your score is {score}, you go together like coke and mentos.")
+# if score >= 40 or score <= 50:
+#     print(f"Your score is {score}, you are alright together.")
+# else:
+#     print(f"Your score is {score}.")
