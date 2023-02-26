37e2d86724147035d528c8aa4788f4efd2c9c043
A-driana <112205906+A-driana@users.noreply.github.com>
2023-02-26 16:45:39 +0100

    Add files via upload

---
+++ heart.py
@@ -0,0 +1,37 @@
+# # TODO Draw beautiful yellow heart with some text
+# import turtle
+# pen = turtle.Turtle()
+#
+#
+# def curve():
+#     for i in range(200):
+#         pen.right(1)
+#         pen.forward(1)
+#
+#
+# def heart():
+#     pen.fillcolor("yellow")
+#     pen.begin_fill()
+#     pen.left(140)
+#     pen.forward(113)
+#     curve()
+#     pen.left(120)
+#     curve()
+#     pen.forward(112)
+#     pen.end_fill()
+#
+#
+# def text():
+#     pen.up(),
+#     pen.setpos(-68, 95)
+#     pen.down()
+#     pen.color("black")
+#     pen.write("Hello, World!", font=("Verdana", 15, "bold"))
+#
+#
+# heart()
+# text()
+# pen.ht()
+# turtle.done()
+#
+# print()
